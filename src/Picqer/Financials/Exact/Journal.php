afbe97ce1d8057f297b9dd72d2897f5f9f2992b4
Stephan Groen <stephan@picqer.com>
2015-06-16 11:56:06 +0200

    Added journals

---
+++ src/Picqer/Financials/Exact/Journal.php
@@ -0,0 +1,18 @@
+<?php namespace Picqer\Financials\Exact;
+
+class Journal extends Model {
+
+    use Query\Findable;
+    use Persistance\Storable;
+
+    protected $fillable = [
+        'Code',
+        'Division',
+        'Description',
+        'ID',
+        'Type',
+    ];
+
+    protected $url = 'financial/Journals';
+
+}
