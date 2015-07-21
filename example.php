9e219203979a9d8f8b0fb006de3504038b9fd168
Stephan Groen <stephan@picqer.com>
2015-07-21 16:26:13 +0200

    Update to Guzzle 6, redone oAuth2 stuff to have no further dependencies, requires php 5.5+

--- example.php
+++ example.php
@@ -4,76 +4,85 @@ require 'vendor/autoload.php';
 
 function getValue($key)
 {
-    $storage = json_decode(file_get_contents('storage.txt'), true);
-    if (array_key_exists($key, $storage)) return $storage[$key];
+    $storage = json_decode(file_get_contents('storage.json'), true);
+    if (array_key_exists($key, $storage)) {
+        return $storage[$key];
+    }
     return null;
 }
 
 function setValue($key, $value)
 {
-    $storage = json_decode(file_get_contents('storage.txt'), true);
+    $storage       = json_decode(file_get_contents('storage.json'), true);
     $storage[$key] = $value;
-    file_put_contents('storage.txt', json_encode($storage));
+    file_put_contents('storage.json', json_encode($storage));
 }
 
 function authorize()
 {
     $connection = new \Picqer\Financials\Exact\Connection();
-    $connection->setRedirectUrl('URL');
-    $connection->setExactClientId('CLIENTID');
-    $connection->setExactClientSecret('SECRET');
+    $connection->setRedirectUrl('YOUR_REDIRECT_URL');
+    $connection->setExactClientId('YOUR_CLIENT_ID');
+    $connection->setExactClientSecret('YOUR_CLIENT_SECRET');
     $connection->redirectForAuthorization();
 }
 
 function connect()
 {
     $connection = new \Picqer\Financials\Exact\Connection();
-    $connection->setRedirectUrl('URL');
-    $connection->setExactClientId('CLIENTID');
-    $connection->setExactClientSecret('SECRET');
+    $connection->setRedirectUrl('YOUR_REDIRECT_URL');
+    $connection->setExactClientId('YOUR_CLIENT_ID');
+    $connection->setExactClientSecret('YOUR_CLIENT_SECRET');
 
     if (getValue('authorizationcode')) // Retrieves authorizationcode from database
+    {
         $connection->setAuthorizationCode(getValue('authorizationcode'));
+    }
 
     if (getValue('accesstoken')) // Retrieves accesstoken from database
-        $connection->setAccessToken(unserialize(getValue('accesstoken')));
+    {
+        $connection->setAccessToken(getValue('accesstoken'));
+    }
 
     if (getValue('refreshtoken')) // Retrieves refreshtoken from database
+    {
         $connection->setRefreshToken(getValue('refreshtoken'));
+    }
+
+    if (getValue('expires_in')) // Retrieves expires from database
+    {
+        $connection->setTokenExpires(getValue('expires_in'));
+    }
 
     // Make the client connect and exchange tokens
     try {
-        $connection->client();
-    } catch (\Exception $e)
-    {
+        $connection->connect();
+    } catch (\Exception $e) {
         throw new Exception('Could not connect to Exact: ' . $e->getMessage());
     }
 
     // Save the new tokens for next connections
-    setValue('accesstoken', serialize($connection->getAccessToken()));
+    setValue('accesstoken', $connection->getAccessToken());
     setValue('refreshtoken', $connection->getRefreshToken());
+    setValue('expires_in', $connection->getTokenExpires());
 
     return $connection;
 }
 
-if (isset($_GET['code']) && is_null(getValue('authorizationcode')))
-{
+if (isset($_GET['code']) && is_null(getValue('authorizationcode'))) {
     setValue('authorizationcode', $_GET['code']);
 }
 
-if (is_null(getValue('authorizationcode')))
-{
-    authorize();
-    exit;
-} else {
-    $connection = connect();
-}
+$connection = connect();
 
-$journals = new \Picqer\Financials\Exact\Journal($connection);
-$result = $journals->get();
-foreach ($result as $journal)
-{
-    echo 'journal: ' . $journal->Description . '<br>';
-}
+try {
+    $journals = new \Picqer\Financials\Exact\Journal($connection);
+    $result   = $journals->get();
+    foreach ($result as $journal) {
+        echo 'journal: ' . $journal->Description . '<br>';
+    }
 
-echo 'done';
+    echo 'done';
+} catch (\Exception $e) {
+    echo get_class($e) . ' : ' . $e->getMessage();
+}
--- src/Picqer/Financials/Exact/Connection.php
+++ src/Picqer/Financials/Exact/Connection.php
@@ -1,18 +1,62 @@
 <?php namespace Picqer\Financials\Exact;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
 
-class Connection {
+/**
+ * Class Connection
+ * @package Picqer\Financials\Exact
+ */
+class Connection
+{
 
+    /**
+     * @var string
+     */
     private $apiUrl = 'https://start.exactonline.nl/api/v1';
+    /**
+     * @var string
+     */
     private $authUrl = 'https://start.exactonline.nl/api/oauth2/auth';
+    /**
+     * @var string
+     */
     private $tokenUrl = 'https://start.exactonline.nl/api/oauth2/token';
 
+    /**
+     * @var
+     */
     private $exactClientId;
+    /**
+     * @var
+     */
     private $exactClientSecret;
+    /**
+     * @var
+     */
     private $authorizationCode;
+    /**
+     * @var
+     */
     private $accessToken;
+
+    /**
+     * @var
+     */
+    private $tokenExpires;
+
+    /**
+     * @var
+     */
     private $refreshToken;
+    /**
+     * @var
+     */
     private $redirectUrl;
+    /**
+     * @var
+     */
     private $division;
 
     /**
@@ -20,72 +64,152 @@ class Connection {
      */
     private $client;
 
-    public function client()
+    /**
+     * @return Client
+     */
+    private function client()
     {
-        if ($this->client) return $this->client;
+        if ($this->client) {
+            return $this->client;
+        }
 
-        $this->client = new Client($this->apiUrl);
-        $this->client->setDefaultOption('headers/Accept', 'application/json');
-        $this->client->setDefaultOption('headers/Content-Type', 'application/json');
-
-        $this->authenticate();
+        $this->client = new Client([
+            'http_errors' => true,
+        ]);
 
         return $this->client;
     }
 
+    public function connect()
+    {
+        // Redirect for authorization if needed (no access token or refresh token given)
+        if ($this->needsAuthentication()) {
+            $this->redirectForAuthorization();
+        }
+
+        // If access token is not set or token has expired, acquire new token
+        if (empty($this->accessToken) || $this->tokenHasExpired()) {
+            $this->acquireAccessToken();
+        }
+
+        $client = $this->client();
+
+        return $client;
+    }
+
+    /**
+     * @param string $method
+     * @param $endpoint
+     * @param null $body
+     * @param array $params
+     * @param array $headers
+     * @return Request
+     */
+    private function createRequest($method = 'GET', $endpoint, $body = null, array $params = [], array $headers = [])
+    {
+        // Add default json headers to the request
+        $headers = array_merge($headers, [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json'
+        ]);
+
+        // If access token is not set or token has expired, acquire new token
+        if (empty($this->accessToken) || $this->tokenHasExpired()) {
+            $this->acquireAccessToken();
+        }
+
+        // If we have a token, sign the request
+        if (!$this->needsAuthentication() && !empty($this->accessToken)) {
+            $headers['Authorization'] = 'Bearer ' . $this->accessToken;
+        }
+
+        // Create param string
+        if (!empty($params)) {
+            $endpoint .= '?' . http_build_query($params);
+        }
+
+        // Create the request
+        $request = new Request($method, $endpoint, $headers, $body);
+
+        return $request;
+    }
+
+    /**
+     * @param $url
+     * @param array $params
+     * @return mixed
+     * @throws ApiException
+     */
     public function get($url, array $params = [])
     {
-        if ($url !== 'current/Me') $this->addDivisionNumberToApiUrl();
+        $url = $this->formatUrl($url, $url !== 'current/Me');
 
-        $request = $this->client()->createRequest('GET', $url);
+        $request = $this->createRequest('GET', $url, null, $params);
 
-        $query = $request->getQuery();
+        $response = $this->client()->send($request);
 
-        foreach ($params as $paramName => $paramValue)
-        {
-            $query->set($paramName, $paramValue);
-        }
-
-        $result = $this->client()->send($request);
-
-        return $this->parseResult($result);
+        return $this->parseResponse($response);
     }
 
+    /**
+     * @param $url
+     * @param $body
+     * @return mixed
+     * @throws ApiException
+     */
     public function post($url, $body)
     {
-        $this->addDivisionNumberToApiUrl();
-        try
-        {
-            $result = $this->client()->post($url, null, $body)->send();
-        } catch (ServerErrorResponseException $e)
-        {
-            throw new ApiException($e->getResponse()->getBody(true));
+        $url = $this->formatUrl($url);
+
+        try {
+            $request  = $this->createRequest('POST', $url, $body);
+            $response = $this->client()->send($request);
+        } catch (\Exception $e) {
+            throw new ApiException($e->getMessage());
         }
 
-        return $this->parseResult($result);
+        return $this->parseResponse($response);
     }
 
+    /**
+     * @param $url
+     * @param $body
+     * @return mixed
+     * @throws ApiException
+     */
     public function put($url, $body)
     {
-        $this->addDivisionNumberToApiUrl();
-        $result = $this->client()->put($url, null, $body)->send();
+        $url = $this->formatUrl($url);
 
-        return $this->parseResult($result);
+        $request  = $this->createRequest('PUT', $url, $body);
+        $response = $this->client()->send($request);
+
+        return $this->parseResponse($response);
     }
 
+    /**
+     * @param $url
+     * @return mixed
+     * @throws ApiException
+     */
     public function delete($url)
     {
-        $this->addDivisionNumberToApiUrl();
-        $result = $this->client()->delete($url)->send();
+        $url = $this->formatUrl($url);
 
-        return $this->parseResult($result);
+        $request  = $this->createRequest('DELETE', $url);
+        $response = $this->client()->send($request);
+
+        return $this->parseResponse($response);
     }
 
-    public function getAuthUrl()
+    /**
+     * @return string
+     */
+    private function getAuthUrl()
     {
         return $this->authUrl . '?' . http_build_query(array(
-            'client_id'     => $this->exactClientId,
-            'redirect_uri'  => $this->redirectUrl,
+            'client_id' => $this->exactClientId,
+            'redirect_uri' => $this->redirectUrl,
             'response_type' => 'code'
         ));
     }
@@ -130,36 +254,14 @@ class Connection {
         $this->refreshToken = $refreshToken;
     }
 
-    public function authenticate()
-    {
-        $params = array(
-            'code'          => $this->authorizationCode,
-            'redirect_uri'  => $this->redirectUrl,
-            'client_id'     => $this->exactClientId,
-            'client_secret' => $this->exactClientSecret,
-            'refresh_token' => $this->refreshToken
-        );
 
-        $oauth2Client = new Client($this->tokenUrl);
-        $grantType = new AuthorizationCode($oauth2Client, $params);
-        $refreshTokenGrantType = new RefreshToken($oauth2Client, $params);
-        $oauth2Plugin = new Oauth2Plugin($grantType, $refreshTokenGrantType);
-        if (! empty($this->refreshToken))
-            $oauth2Plugin->setRefreshToken($this->refreshToken);
-
-        $this->accessToken = $oauth2Plugin->getAccessToken();
-        $this->refreshToken = $oauth2Plugin->getRefreshToken();
-
-        $this->client()->addSubscriber($oauth2Plugin);
-    }
-
+    /**
+     *
+     */
     public function redirectForAuthorization()
     {
-        header('Location: ' . $this->getAuthUrl(
-                $this->exactClientId,
-                $this->redirectUrl
-            )
-        );
+        $authUrl = $this->getAuthUrl();
+        header('Location: ' . $authUrl);
         exit;
     }
 
@@ -171,22 +273,26 @@ class Connection {
         $this->redirectUrl = $redirectUrl;
     }
 
+    /**
+     * @return bool
+     */
     public function needsAuthentication()
     {
         return empty($this->refreshToken) && empty($this->authorizationCode);
     }
 
-    public function parseResult(Response $response)
+    /**
+     * @param Response $response
+     * @return mixed
+     * @throws ApiException
+     */
+    private function parseResponse(Response $response)
     {
-        try
-        {
-            $json = $response->json();
-            if (array_key_exists('d', $json))
-            {
-                if (array_key_exists('results', $json['d']))
-                {
-                    if (count($json['d']['results']) == 1)
-                    {
+        try {
+            $json = json_decode($response->getBody()->getContents(), true);
+            if (array_key_exists('d', $json)) {
+                if (array_key_exists('results', $json['d'])) {
+                    if (count($json['d']['results']) == 1) {
                         return $json['d']['results'][0];
                     }
 
@@ -197,20 +303,22 @@ class Connection {
             }
 
             return $json;
-        } catch (\RuntimeException $e)
-        {
+        } catch (\RuntimeException $e) {
             throw new ApiException($e->getMessage());
         }
     }
 
-    public function addDivisionNumberToApiUrl()
+    /**
+     * @return mixed
+     */
+    private function getCurrentDivisionNumber()
     {
-        if (empty($this->division))
-        {
-            $me = new Me($this);
+        if (empty($this->division)) {
+            $me             = new Me($this);
             $this->division = $me->find()->CurrentDivision;
-            $this->client()->setBaseUrl($this->client()->getBaseUrl() . '/' . $this->division);
         }
+
+        return $this->division;
     }
 
     /**
@@ -227,6 +335,98 @@ class Connection {
     public function getAccessToken()
     {
         return $this->accessToken;
+    }
+
+    private function acquireAccessToken()
+    {
+        // If refresh token not yet acquired, do token request
+        if (empty($this->refreshToken)) {
+            $body = [
+                'form_params' => [
+                    'redirect_uri' => $this->redirectUrl,
+                    'grant_type' => 'authorization_code',
+                    'client_id' => $this->exactClientId,
+                    'client_secret' => $this->exactClientSecret,
+                    'code' => $this->authorizationCode
+                ]
+            ];
+        } else { // else do refresh token request
+            $body = [
+                'form_params' => [
+                    'refresh_token' => $this->refreshToken,
+                    'grant_type' => 'refresh_token',
+                    'client_id' => $this->exactClientId,
+                    'client_secret' => $this->exactClientSecret,
+                ]
+            ];
+        }
+
+        $response = $this->client()->post($this->tokenUrl, $body);
+
+        if ($response->getStatusCode() == 200) {
+            $body = json_decode($response->getBody()->getContents(), true);
+
+            if (json_last_error() === JSON_ERROR_NONE) {
+                $this->accessToken  = $body['access_token'];
+                $this->refreshToken = $body['refresh_token'];
+                $this->tokenExpires = $this->getDateTimeFromExpires($body['expires_in']);
+            } else {
+                throw new ApiException('Could not acquire tokens, json decode failed. Got response: ' . $response->getBody()->getContents());
+            }
+        } else {
+            throw new ApiException('Could not acquire or refresh tokens');
+        }
+    }
+
+
+    private function getDateTimeFromExpires($expires)
+    {
+        if (!is_numeric($expires)) {
+            throw new \InvalidArgumentException('Function requires a numeric expires value');
+        }
+
+        return time() + 600;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTokenExpires()
+    {
+        return $this->tokenExpires;
+    }
+
+    /**
+     * @param mixed $tokenExpires
+     */
+    public function setTokenExpires($tokenExpires)
+    {
+        $this->tokenExpires = $tokenExpires;
+    }
+
+    private function tokenHasExpired()
+    {
+        if (empty($this->tokenExpires)) {
+            return true;
+        }
+
+        return $this->tokenExpires <= time();
+    }
+
+    private function formatUrl($endPoint, $includeDivision = true)
+    {
+        if ($includeDivision) {
+            return implode('/', [
+                $this->apiUrl,
+                $this->getCurrentDivisionNumber(),
+                $endPoint
+            ]);
+        }
+
+        return implode('/', [
+            $this->apiUrl,
+            $endPoint
+        ]);
     }
 
 }
