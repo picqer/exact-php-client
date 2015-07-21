a08438e301d72f5cae6125ab24e4d537ed2ccf20
Sebastian Berm <sebastian@sebsoft.nl>
2015-07-21 19:14:49 +0200

    Added support for more stuff
    Added support for more Account fields.
    Added support for Contacts.
    Added support for GLAccounts.
    Added support for PaymentConditions.
    Added support for SalesEntry's (sales bookings in the old system)

---
+++ src/Picqer/Financials/Exact/Contact.php
@@ -0,0 +1,32 @@
+<?php namespace Picqer\Financials\Exact;
+
+class Contact extends Model {
+
+    use Query\Findable;
+    use Persistance\Storable;
+
+    protected $fillable = [
+        'Account',
+        'AccountIsCustomer',
+        'AccountIsSupplier',
+	'AccountName',
+	'AddressStreet',
+	'AddressStreetNumber',
+	'FirstName',
+	'LastName',
+	'Phone',
+	'Postcode',
+        'City',
+        'Code',
+        'Country',
+        'Division',
+        'Email',
+        'HID',
+        'AccountMainContact',
+        'IsMainContact'
+    ];
+
+    protected $url = 'crm/Contacts';
+
+}
+
---
+++ src/Picqer/Financials/Exact/SalesEntry.php
@@ -0,0 +1,45 @@
+<?php namespace Picqer\Financials\Exact;
+
+class SalesEntry extends Model {
+
+    use Query\Findable;
+    use Persistance\Storable;
+    
+    protected $primaryKey = 'EntryID';
+    protected $saleEntryLines = array();
+
+    protected $fillable = [
+        'Division',
+        'Currency',
+        'EntryDate',
+        'EntryID',
+        'EntryNumber',
+        'Customer',
+        'YourRef',
+        'SalesEntryLines',
+        'Document',
+        'Journal',
+        'DueDate',
+        'ExternalLinkReference',
+        'ExternalLinkDescription',
+        'Description',
+        'Status',
+        'PaymentCondition'
+    ];
+    
+    public function addItem(array $array)
+    {
+        if (!isset($this->attributes['SalesEntryLines']) || $this->attributes['SalesEntryLines'] == null)
+        {
+            $this->attributes['SalesEntryLines'] = array();
+        }
+        if (!isset($array['LineNumber']))
+        {
+            $array['LineNumber'] = count($this->attributes['SalesEntryLines'])+1;
+        }
+        $this->attributes['SalesEntryLines'][] = $array;
+    }
+
+    protected $url = 'salesentry/SalesEntries';
+
+}
