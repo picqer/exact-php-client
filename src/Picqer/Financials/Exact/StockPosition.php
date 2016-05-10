c328db17008795ad79d573d116942b4687dd27a5
Jan-Marten de Boer <janmarten.deboer@mediact.nl>
2016-05-10 15:11:25 +0200

    Added StockPosition entity

---
+++ src/Picqer/Financials/Exact/StockPosition.php
@@ -0,0 +1,34 @@
+<?php
+namespace Picqer\Financials\Exact;
+
+/**
+ * Entity holding stock position details.
+ *
+ * @package Picqer\Financials\Exact
+ * @see https://start.exactonline.nl/docs/HlpRestAPIResourcesDetails.aspx?name=ReadLogisticsStockPosition
+ */
+class StockPosition extends Model
+{
+    use Query\Findable;
+
+    /**
+     * The fillable properties for the StockPosition model.
+     *
+     * @var string[]
+     */
+    protected $fillable = ['InStock', 'ItemId', 'PlanningIn', 'PlanningOut'];
+
+    /**
+     * The API request URL slug.
+     *
+     * @var string
+     */
+    protected $url = 'logistics/StockPosition';
+
+    /**
+     * The primary key for the current entity.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'ItemId';
+}
