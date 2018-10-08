03edd59f2005f44e7a9b8c9a66bcd93649e69b09
Mark Jongkind <mark@backscreen.nl>
2018-10-08 11:11:15 +0200

    Fix type hints for ShopOrderRoutingStepPlan entity

--- src/Picqer/Financials/Exact/ShopOrderRoutingStepPlan.php
+++ src/Picqer/Financials/Exact/ShopOrderRoutingStepPlan.php
@@ -8,51 +8,51 @@ namespace Picqer\Financials\Exact;
  * @package Picqer\Financials\Exact
  * @see https://start.exactonline.nl/docs/HlpRestAPIResourcesDetails.aspx?name=ManufacturingShopOrderRoutingStepPlans
  *
- * @property guid $ID Primary key
- * @property guid $Account Reference to Account providing the Outsourced item
+ * @property string $ID Primary key
+ * @property string $Account Reference to Account providing the Outsourced item
  * @property string $AccountName Account name
  * @property string $AccountNumber Account number
  * @property double $AttendedPercentage Attended Percentage
  * @property byte $Backflush Indicates if this is a backflush step
  * @property double $CostPerItem Total cost / Shop order planned quantity
- * @property datetime $Created Creation date
- * @property guid $Creator User ID of creator
+ * @property string $Created Creation date
+ * @property string $Creator User ID of creator
  * @property string $CreatorFullName Name of creator
  * @property string $Description Description of the operation
- * @property int32 $Division Division code
+ * @property int $Division Division code
  * @property double $EfficiencyPercentage Efficiency Percentage
- * @property int32 $FactorType Conversion factor type between Shop order Item and Subcontract purchase Unit
- * @property int32 $LineNumber Sequential order of the operation
- * @property datetime $Modified Last modified date
- * @property guid $Modifier User ID of modifier
+ * @property int $FactorType Conversion factor type between Shop order Item and Subcontract purchase Unit
+ * @property int $LineNumber Sequential order of the operation
+ * @property string $Modified Last modified date
+ * @property string $Modifier User ID of modifier
  * @property string $ModifierFullName Name of modifier
  * @property string $Notes Notes
- * @property guid $Operation Reference to Operations
+ * @property string $Operation Reference to Operations
  * @property string $OperationCode Code of the routing step operation
  * @property string $OperationDescription Description of the operation step
- * @property guid $OperationResource Reference to OperationResources
- * @property datetime $PlannedEndDate Planned end date
+ * @property string $OperationResource Reference to OperationResources
+ * @property string $PlannedEndDate Planned end date
  * @property double $PlannedRunHours Planned run hours
  * @property double $PlannedSetupHours Planned setup hours
- * @property datetime $PlannedStartDate Planned start date
+ * @property string $PlannedStartDate Planned start date
  * @property double $PlannedTotalHours Setup hours + Run hours
  * @property string $PurchaseUnit Reference to Units
  * @property double $PurchaseUnitFactor Purchase Unit Factor
  * @property double $PurchaseUnitPriceFC Purchase Unit Price in the currency of the transaction
  * @property double $PurchaseUnitQuantity Purchase unit quantity of the plan
- * @property int32 $RoutingStepType Reference to RoutingStepTypes
+ * @property int $RoutingStepType Reference to RoutingStepTypes
  * @property double $Run Used in conjunction with RunMethod, and EfficiencyPercentage to determine PlannedRunHours
- * @property int32 $RunMethod Reference to OperationMethod
+ * @property int $RunMethod Reference to OperationMethod
  * @property string $RunMethodDescription Description of RunMethod
  * @property double $Setup Used in conjunction with SetupCount and Setup Unit to determine PlannedSetupHours
  * @property string $SetupUnit Reference to TimeUnits
- * @property guid $ShopOrder Reference to Shop orders
- * @property int32 $Status Reference to OperationStatus
+ * @property string $ShopOrder Reference to Shop orders
+ * @property int $Status Reference to OperationStatus
  * @property string $StatusDescription Description of Status
- * @property int32 $SubcontractedLeadDays Subcontracted lead days
- * @property transactions $TimeTransactions Collection of TimeTransactions
+ * @property int $SubcontractedLeadDays Subcontracted lead days
+ * @property timetransactions $TimeTransactions Collection of TimeTransactions
  * @property double $TotalCostDC Total cost of the routing line
- * @property guid $Workcenter Reference to Workcenters
+ * @property string $Workcenter Reference to Workcenters
  * @property string $WorkcenterCode Workcenter code
  * @property string $WorkcenterDescription Workcenter description
  */
@@ -60,8 +60,6 @@ class ShopOrderRoutingStepPlan extends Model
 {
     use Query\Findable;
     use Persistance\Storable;
-
-    protected $primaryKey = 'ID';
 
     protected $fillable = [
         'ID',
