8e03a18d1786d1a262ce421bdac07fcf596ed758
Mark Jongkind <mark@backscreen.nl>
2018-10-23 15:50:37 +0200

    Added deprecated BillOfMaterialMaterials and BillOfMaterialVersions classes for backward compatibility

---
+++ src/Picqer/Financials/Exact/BillOfMaterialMaterials.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Picqer\Financials\Exact;
+
+/**
+ * Added deprecated BillOfMaterialMaterials class for backward compatibility
+ */
+class_alias(__NAMESPACE__ . '\BillOfMaterialMaterial', __NAMESPACE__ . '\BillOfMaterialMaterials');
---
+++ src/Picqer/Financials/Exact/BillOfMaterialVersions.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Picqer\Financials\Exact;
+
+/**
+ * Added deprecated BillOfMaterialVersions class for backward compatibility
+ */
+class_alias(__NAMESPACE__ . '\BillOfMaterialVersion', __NAMESPACE__ . '\BillOfMaterialVersions');
