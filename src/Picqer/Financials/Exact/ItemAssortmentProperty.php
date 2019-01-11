c1eb219a8b34ed4687fccc4c1d98c6d931fb5170
Pim Dröge <pim@improvit.nl>
2019-01-11 09:18:27 +0100

    added ItemAssortiment and ItemAssortimentProperty classes

---
+++ src/Picqer/Financials/Exact/ItemAssortmentProperty.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Picqer\Financials\Exact;
+
+/**
+ * Class ItemAssortmentProperty
+ *
+ * @package Picqer\Financials\Exact
+ * @see https://start.exactonline.nl/docs/HlpRestAPIResourcesDetails.aspx?name=LogisticsItemAssortmentProperty
+ *
+ * @property string $ID ID of Property
+ * @property string $Code Code of Property
+ * @property string $Description Description of Property
+ * @property int $Division Division
+ * @property int $ItemAssortmentCode Code of ItemAssortment
+ */
+class ItemAssortmentProperty extends Model
+{
+    use Query\Findable;
+    use Persistance\Storable;
+
+    protected $fillable = [
+        'ID',
+        'Code',
+        'Description',
+        'Division',
+        'ItemAssortmentCode',
+    ];
+
+    protected $url = 'logistics/ItemAssortmentProperty';
+}
