c51d6cddcd5717f29a3e0f86995fdf310797d350
Stephan Groen <mail@stephangroen.nl>
2019-04-24 18:23:09 +0000

    Apply fixes from StyleCI

--- src/Picqer/Financials/Exact/BulkTransactionLine.php
+++ src/Picqer/Financials/Exact/BulkTransactionLine.php
@@ -2,12 +2,9 @@
 
 namespace Picqer\Financials\Exact;
 
-use Picqer\Financials\Exact\TransactionLine;
-
 /**
- * Class BulkTransactionLine
+ * Class BulkTransactionLine.
  *
- * @package Picqer\Financials\Exact
  * @see https://start.exactonline.nl/docs/HlpRestAPIResourcesDetails.aspx?name=BulkFinancialTransactionLines
  */
 class BulkTransactionLine extends TransactionLine
