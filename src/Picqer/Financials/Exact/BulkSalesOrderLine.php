b321dd7b9f180c2e24d7e1bde82f3edf99748cdc
Jeffrey Slort <jeffrey.slort@allyourbi.nl>
2019-11-08 14:27:24 +0100

    Bulk sales orderline does not have all the properties of the default sales order line endpoint

--- src/Picqer/Financials/Exact/BulkSalesOrderLine.php
+++ src/Picqer/Financials/Exact/BulkSalesOrderLine.php
@@ -7,5 +7,48 @@ namespace Picqer\Financials\Exact;
  */
 class BulkSalesOrderLine extends SalesOrderLine
 {
+    protected $fillable = [
+        'ID',
+        'AmountDC',
+        'AmountFC',
+        'CostCenter',
+        'CostCenterDescription',
+        'CostPriceFC',
+        'CostUnit',
+        'CostUnitDescription',
+        'DeliveryDate',
+        'Description',
+        'Discount',
+        'Division',
+        'Item',
+        'ItemCode',
+        'ItemDescription',
+        'ItemVersion',
+        'ItemVersionDescription',
+        'LineNumber',
+        'NetPrice',
+        'Notes',
+        'OrderID',
+        'OrderNumber',
+        'Pricelist',
+        'PricelistDescription',
+        'Project',
+        'ProjectDescription',
+        'PurchaseOrder',
+        'PurchaseOrderLine',
+        'PurchaseOrderLineNumber',
+        'PurchaseOrderNumber',
+        'Quantity',
+        'ShopOrder',
+        'UnitCode',
+        'UnitDescription',
+        'UnitPrice',
+        'UseDropShipment',
+        'VATAmount',
+        'VATCode',
+        'VATCodeDescription',
+        'VATPercentage',
+    ];
+    
     protected $url = 'bulk/SalesOrder/SalesOrderLines';
 }
