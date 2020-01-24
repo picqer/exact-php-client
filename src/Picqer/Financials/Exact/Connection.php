e9b2af37948d08775c5a2e209ac9da135a44e8d2
Michiel Vugteveen <info@it-can.nl>
2020-01-24 18:59:00 +0100

    fix phpdoc

--- src/Picqer/Financials/Exact/Connection.php
+++ src/Picqer/Financials/Exact/Connection.php
@@ -155,6 +155,7 @@ class Connection
 
     /**
      * Insert a custom Guzzle client
+     *
      * @param Client $client
      */
     public function setClient($client)
@@ -164,6 +165,7 @@ class Connection
 
      /**
      * Insert a Middleware for the Guzzle Client.
+     *
      * @param $middleWare
      */
     public function insertMiddleWare($middleWare)
@@ -172,8 +174,9 @@ class Connection
     }
 
     /**
-     * @return Client
      * @throws ApiException
+     *
+     * @return Client
      */
     public function connect()
     {
