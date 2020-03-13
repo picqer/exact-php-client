88af8bc4e6af0e24f54909b582bb2338e74b66db
Stephan Groen <stephangroen@users.noreply.github.com>
2020-03-13 13:57:14 +0100

    Style fix

--- src/Picqer/Financials/Exact/Task.php
+++ src/Picqer/Financials/Exact/Task.php
@@ -16,7 +16,6 @@ namespace Picqer\Financials\Exact;
  * @property guid $Modifier User ID of the last modifier
  * @property string $Notes The notes of the task
  * @property guid $User The user that has to realize the task
- *
  */
 class Task extends Model
 {
