2051fef01955138a0363d54550cabe5d64dcb86a
Stephan Groen <mail@stephangroen.nl>
2020-03-13 15:12:10 +0100

    Alias for ReceivableList

---
+++ src/Picqer/Financials/Exact/ReceivableList.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Picqer\Financials\Exact;
+
+/*
+ * Added deprecated ReceivableList class for backward compatibility
+ */
+class_alias(__NAMESPACE__ . '\ReceivablesList', __NAMESPACE__ . '\ReceivableList');
