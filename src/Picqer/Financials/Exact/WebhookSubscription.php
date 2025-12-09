740319992ae1fb43838e145ec76f4f7ba062a499
Dennis van Barneveld <d.vanbarneveld@sendy.nl>
2025-12-09 13:51:54 +0100

    Add IsInstant property to WebhookSubscriptions

--- src/Picqer/Financials/Exact/WebhookSubscription.php
+++ src/Picqer/Financials/Exact/WebhookSubscription.php
@@ -15,6 +15,7 @@ namespace Picqer\Financials\Exact;
  * @property string $CreatorFullName Name of creator
  * @property string $Description Description of the OAuth Client
  * @property int $Division Division code
+ * @property bool $IsInstant Enables Instant delivery (only supported for the Good Deliveries topic)
  * @property string $Topic Webhook subscription topic, e.g.: FinancialTransactions, Items, StockPositions
  * @property string $UserID Subscribing User ID
  */
@@ -32,6 +33,7 @@ class WebhookSubscription extends Model
         'CreatorFullName',
         'Description',
         'Division',
+        'IsInstant',
         'Topic',
         'UserID',
     ];
